86f4be89b2bfea34b36b453b93eab0c35c0ce87b
JoaoRozestraten <joaorozestraten@gmail.com>
2024-09-22 17:47:27 -0300

    aa

--- A_Teste/TITLE teste.asm
+++ A_Teste/TITLE teste.asm
@@ -9,7 +9,7 @@ main PROC
     mov ds, ax
     mov ax, 0000h
     mov bx, offset m1
-    mov ah, [bx]
+    mov ah, [bx]; offset ds:bx 2 de 16 --> 20
     inc bx
     mov ah, [bx]
     inc bx
@@ -20,10 +20,11 @@ main PROC
     L1:
     dec bx
     dec cx
-    jnz L1
+    jnz L1;teste de uso de LOOP --> poderia substituir por mov bx 4
+    ;MAS assim nao acessa a memoria 
     mov al, [bx]
     dec bx
-    mov al, [bx]
+    mov al, [bx] ;1234 em ax
     mov ah,4ch
     int 21h
 main ENDP
